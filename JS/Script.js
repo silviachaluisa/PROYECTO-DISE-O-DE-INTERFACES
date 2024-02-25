bdcd0efe186d6a0dedfa84afa9b9dc4cfa7f57b9
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-25 17:55:37 -0500

    Bievenido

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,16 @@
+//Boton Cancelar
 function Regresar(){
     window.location.href="Home.html";
 }
 function redirigirPagina(){
-    window.location.href="Inicio.html";
+    window.location.href="Bienvenido.html";
 
+}
+// Boton Registrarse
+function Volver(){
+    window.location.href="IniciarSesion.html";
+}
+//Boton Salir
+function salir(){
+    window.location.href="Home.html";
 }
