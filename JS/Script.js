fd6c0fa4e9e2369b545b69555308d93f0fcfde53
silviachaluisa <veronica-14-1998@hotmail.com>
2024-02-26 00:21:52 -0500

    Sigo cambiando

--- JS/Script.js
+++ JS/Script.js
@@ -14,3 +14,21 @@ function Volver(){
 function salir(){
     window.location.href="Home.html";
 }
+//Boton del Menu Hamburguesa
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
